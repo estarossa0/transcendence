b4bdffbc2395631033a25c3f9a840dbbb94501c3
esta-rossa <rajiabdou9@gmail.com>
2021-10-04 09:19:09 +0100

    fix: change default `Link` theme

--- theme.ts
+++ theme.ts
@@ -1,17 +1,24 @@
 import { extendTheme } from '@chakra-ui/react';
 
+const noneVariant = {
+  none: {
+    _focus: {
+      outline: 'none',
+      boxShadow: 'none',
+      colorScheme: 'none',
+    },
+  },
+};
 const theme = extendTheme({
   components: {
     Button: {
-      variants: {
-        none: {
-          _focus: {
-            outline: 'none',
-            boxShadow: 'none',
-            colorScheme: 'none',
-          },
-        },
+      variants: noneVariant,
+      defaultProps: {
+        variant: 'none',
       },
+    },
+    Link: {
+      variants: noneVariant,
       defaultProps: {
         variant: 'none',
       },
