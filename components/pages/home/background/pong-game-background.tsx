4c0b4b721b353d312991f754100faa662195f93b
esta-rossa <rajiabdou9@gmail.com>
2021-10-12 11:02:22 +0100

    feat: add game ball

--- components/pages/home/background/pong-game-background.tsx
+++ components/pages/home/background/pong-game-background.tsx
@@ -2,6 +2,7 @@ import player1 from '../../../../public/player1.png';
 import player2 from '../../../../public/player2.png';
 import Image from 'next/image';
 import { Box } from '@chakra-ui/react';
+import Ball from './ball';
 
 function PongGameBackground() {
   const playersSize = '460px';
@@ -26,6 +27,7 @@ function PongGameBackground() {
       >
         <Image layout="responsive" src={player2} />
       </Box>
+      <Ball w="100px" pos="absolute" top="50%" left="50%" />
     </Box>
   );
 }
