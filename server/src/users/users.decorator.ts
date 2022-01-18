6611dee3a3dd1fa75f44af57ba36eb7e8d010c4c
esta-rossa <rajiabdou9@gmail.com>
2022-01-18 23:33:27 +0100

    feat[server](user): create decorator to add user to request

---
+++ server/src/users/users.decorator.ts
@@ -0,0 +1,9 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { GqlExecutionContext } from "@nestjs/graphql";
+
+export const CurrentUser = createParamDecorator(
+  (_: unknown, context: ExecutionContext) => {
+    const ctx = GqlExecutionContext.create(context);
+    return ctx.getContext().req.user;
+  },
+);
