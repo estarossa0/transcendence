570d709fbd4e4f8e3345e657f68acad5cbe42233
esta-rossa <rajiabdou9@gmail.com>
2022-01-19 12:23:46 +0100

    feat[server](user): add a `me` query to get current logged user

--- server/src/users/users.resolver.ts
+++ server/src/users/users.resolver.ts
@@ -1,7 +1,9 @@
 import { NotFoundException, UseGuards } from "@nestjs/common";
 import { Args, Query, Resolver } from "@nestjs/graphql";
 import { GqlAuthGuard } from "src/auth/auth.guard";
+import { requestUser } from "./dto/user-types";
 import { user } from "./models/users.model";
+import { CurrentUser } from "./users.decorator";
 import { UsersService } from "./users.service";
 
 @Resolver(() => user)
@@ -12,6 +14,13 @@ export class UserResolver {
   @Query(() => user, { nullable: true })
   async user(@Args("id") id: string) {
     const user = await this.userService.getOne({ id: id });
+    if (!user) throw new NotFoundException();
+    return user;
+  }
+
+  @Query(() => user)
+  async me(@CurrentUser() reqUser: requestUser) {
+    const user = await this.userService.getOne({ id: reqUser.sub });
     if (!user) throw new NotFoundException();
     return user;
   }
