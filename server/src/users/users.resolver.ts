a6241b78c5c774bb0b2a04a607d1009bfae6dd6e
esta-rossa <rajiabdou9@gmail.com>
2022-01-19 12:57:06 +0100

    feat[server](user): add a mutation to update user `displayname`

--- server/src/users/users.resolver.ts
+++ server/src/users/users.resolver.ts
@@ -1,5 +1,10 @@
-import { NotFoundException, UseGuards } from "@nestjs/common";
-import { Args, Query, Resolver } from "@nestjs/graphql";
+import {
+  InternalServerErrorException,
+  NotFoundException,
+  UseGuards,
+} from "@nestjs/common";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 import { GqlAuthGuard } from "src/auth/auth.guard";
 import { requestUser } from "./dto/user-types";
 import { user } from "./models/users.model";
@@ -22,6 +27,24 @@ export class UserResolver {
   async me(@CurrentUser() reqUser: requestUser) {
     const user = await this.userService.getOne({ id: reqUser.sub });
     if (!user) throw new NotFoundException();
+    return user;
+  }
+
+  @Mutation(() => user)
+  async changeDisplayName(
+    @Args("newName") newName: string,
+    @CurrentUser() reqUser: requestUser,
+  ) {
+    const user = this.userService
+      .updateUser({
+        where: { id: reqUser.sub },
+        data: { name: { set: newName } },
+      })
+      .catch((err: PrismaClientKnownRequestError) => {
+        throw new InternalServerErrorException(
+          `failed to update with prisma, code: ${err.code}`,
+        );
+      });
     return user;
   }
 }
