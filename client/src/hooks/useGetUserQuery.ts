39e686c6e63b9357d985e22ee08aacadfc9f17fe
esta-rossa <rajiabdou9@gmail.com>
2022-01-21 20:43:43 +0100

    feat[client](urql): add `getUserQuery` hook

---
+++ client/src/hooks/useGetUserQuery.ts
@@ -0,0 +1,55 @@
+import gql from "graphql-tag";
+import * as Urql from "urql";
+
+type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+/** All built-in and custom scalars, mapped to their actual values */
+type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type User = {
+  __typename?: "user";
+  id: Scalars["String"];
+  displayName: Scalars["String"];
+};
+
+type GetUserQueryVariables = Exact<{
+  userId: Scalars["String"];
+}>;
+
+type GetUserQuery = {
+  __typename?: "Query";
+  user?:
+    | { __typename?: "user"; id: string; displayName: string }
+    | null
+    | undefined;
+};
+
+const UserFieldsFragmentDoc = gql`
+  fragment UserFields on user {
+    id
+    displayName
+  }
+`;
+const GetUserDocument = gql`
+  query getUser($userId: String!) {
+    user(id: $userId) {
+      ...UserFields
+    }
+  }
+  ${UserFieldsFragmentDoc}
+`;
+
+export function useGetUserQuery(
+  options: Omit<Urql.UseQueryArgs<GetUserQueryVariables>, "query"> = {},
+) {
+  return Urql.useQuery<GetUserQuery>({ query: GetUserDocument, ...options });
+}
