dec831ed6ed16eaf787fc6f5b6223f315073371c
esta-rossa <rajiabdou9@gmail.com>
2022-02-06 23:24:42 +0100

    fix[server](user): get user `id` from request instead param

--- server/src/users/users.decorator.ts
+++ server/src/users/users.decorator.ts
@@ -2,8 +2,10 @@ import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 import { GqlExecutionContext } from "@nestjs/graphql";
 
 export const CurrentUser = createParamDecorator(
-  (_: unknown, context: ExecutionContext) => {
+  (param: string, context: ExecutionContext) => {
     const ctx = GqlExecutionContext.create(context);
-    return ctx.getContext().req.user;
+    const user = ctx.getContext().req.user;
+    if (param) return user[param];
+    return user;
   },
 );
