5afea64567e5c7710df30a44d0fe3e8b5aaef58f
esta-rossa <rajiabdou9@gmail.com>
2022-02-06 20:52:51 +0100

    refactor[client](profile): move all `displayname` components to files

--- client/src/components/pages/profile/display-name/display-name-form.tsx
+++ client/src/components/pages/profile/display-name/display-name-form.tsx
@@ -1,27 +1,10 @@
-import {
-  Box,
-  Flex,
-  Text,
-  Button,
-  Skeleton,
-  Input,
-  useBoolean,
-  FormControl,
-} from "@chakra-ui/react";
+import { Flex, Input, useBoolean, FormControl } from "@chakra-ui/react";
 import React, { useEffect, useRef, useState } from "react";
-import { ErrorMessage, Field, Form, Formik } from "formik";
+import { Field, Form, Formik } from "formik";
 import * as Yup from "yup";
 import { User } from "hooks";
-
-const ErrorText = ({ children }) => (
-  <Text fontSize="12px" color="red" m="0">
-    {children}
-  </Text>
-);
-
-const ErrorBox = ({ name }) => (
-  <ErrorMessage name={name} component={ErrorText} />
-);
+import ErrorBox from "./error-box";
+import DisplayNameUpdateButton from "./display-name-button";
 
 const DisplayNameFormik: React.FC<{
   user: User | null;
@@ -85,28 +68,6 @@ const DiplayNameFormContainer: React.FC = ({ children }) => (
   </Flex>
 );
 
-const DisplayNameUpdateButton: React.FC<{
-  isUpdating: boolean;
-  inputRef: React.MutableRefObject<HTMLInputElement | undefined>;
-  on: () => void;
-}> = ({ isUpdating, inputRef, on }) => (
-  <Button
-    onClick={() => {
-      on();
-      if (inputRef.current) {
-        inputRef.current.selectionStart = inputRef.current?.value.length;
-        inputRef.current.selectionEnd = inputRef.current?.value.length;
-      }
-      inputRef.current?.focus();
-    }}
-    size="sm"
-    textColor={isUpdating ? "#0496FF" : "#FF0062"}
-    _active={{}}
-  >
-    {isUpdating ? "Updating" : "Update"}
-  </Button>
-);
-
 const DisplayNameBoxForm = ({ user }: { user: User | null }) => {
   const [isUpdating, { on, off }] = useBoolean(false);
   const inputRef = useRef<HTMLInputElement>();
@@ -138,19 +99,4 @@ const DisplayNameBoxForm = ({ user }: { user: User | null }) => {
   );
 };
 
-const DisplayNameBox = ({ user }: { user: User | null }) => {
-  const Container = user ? Box : Skeleton;
-
-  return (
-    <Box mt="25px" w="full">
-      <Text fontSize="20px" color="#0496FF" fontFamily="ubuntu">
-        Display name
-      </Text>
-      <Container w="70%" rounded="md">
-        <DisplayNameBoxForm user={user} />
-      </Container>
-    </Box>
-  );
-};
-
-export { DisplayNameBox as default };
+export { DisplayNameBoxForm as default };
