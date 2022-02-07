7ab58f3210803a8a4d2a38f4933cc449885f1db4
esta-rossa <rajiabdou9@gmail.com>
2022-02-07 17:06:18 +0100

    feat[server] serve static files in `public` folder

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
 import { graphqlUploadExpress } from "graphql-upload";
+import { join } from "path";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 1 }));
+  app.useStaticAssets(join(__dirname, "..", "public"), { prefix: "/assets" });
   await app.listen(3000);
 }
 bootstrap();
